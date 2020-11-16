6cef287968911101ec25dae3af964cb63138a3c1
Alastair Byrne <alastairbyrne@gmail.com>
2020-11-16 14:00:57 +0000

    Initial Commit

---
+++ Big Sur Font Smoothing Toggler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Big Sur Font Smoothing Toggler
+//
+//  Created by Alastair Byrne on 16/11/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Big Sur Font Smoothing TogglerTests/Big_Sur_Font_Smoothing_TogglerTests.swift
@@ -0,0 +1,33 @@
+//
+//  Big_Sur_Font_Smoothing_TogglerTests.swift
+//  Big Sur Font Smoothing TogglerTests
+//
+//  Created by Alastair Byrne on 16/11/2020.
+//
+
+import XCTest
+@testable import Big_Sur_Font_Smoothing_Toggler
+
+class Big_Sur_Font_Smoothing_TogglerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
