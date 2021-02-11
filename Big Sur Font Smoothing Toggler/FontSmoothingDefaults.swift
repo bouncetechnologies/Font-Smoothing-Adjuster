81ae8fb4005f6865b35417a9679a7a66b1b2ef41
Alastair Byrne <alastairbyrne@gmail.com>
2021-02-11 12:31:27 +0000

    Refactor to use CoreFoundation functions instead of spawning processes to modify defaults

--- Big Sur Font Smoothing Toggler/FontSmoothingDefaults.swift
+++ Big Sur Font Smoothing Toggler/FontSmoothingDefaults.swift
@@ -9,98 +9,48 @@ import Foundation
 import os.log
 
 class FontSmoothingDefaults {
-    
-    private let defaultsUrl = URL(fileURLWithPath: "/usr/bin/defaults")
-    private let domainDefaultPairDoesNotExistText = "The domain/default pair of (kCFPreferencesAnyApplication, AppleFontSmoothing) does not exist"
-    private typealias ProcessArguments = [String]
-    
-    private let setFontSmoothingStateArguments: [FontSmoothingOptions : ProcessArguments] = [
-        .defaultFontSmoothing : ["-currentHost", "delete", "-g", "AppleFontSmoothing"],
-        .noFontSmoothing : ["-currentHost", "write", "-g", "AppleFontSmoothing", "-int", "0"],
-        .lightFontSmoothing : ["-currentHost", "write", "-g", "AppleFontSmoothing", "-int", "1"],
-        .mediumFontSmoothing : ["-currentHost", "write", "-g", "AppleFontSmoothing", "-int", "2"],
-        .heavyFontSmoothing : ["-currentHost", "write", "-g", "AppleFontSmoothing", "-int", "3"],
-    ]
-    
-    private let getFontSmoothingStateArguments = ["-currentHost", "read", "Apple Global Domain", "AppleFontSmoothing"]
-    
-    enum FontSmoothingOptions {
-        case defaultFontSmoothing
-        case lightFontSmoothing
-        case mediumFontSmoothing
-        case heavyFontSmoothing
-        case noFontSmoothing
+        
+    enum FontSmoothingOptions: Int {
+        case noFontSmoothing = 0
+        case lightFontSmoothing = 1
+        case mediumFontSmoothing = 2
+        case heavyFontSmoothing = 3
     }
     
-    struct DefaultsResult {
-        let output: String
-        let error: String
+    func setFontSmoothing(option: FontSmoothingOptions) throws {
+        let value = option.rawValue as CFNumber
+        let key = CFPreferencesConstants.key
+        let applicationID = CFPreferencesConstants.applicationID
+        let userName = CFPreferencesConstants.userName
+        let hostName = CFPreferencesConstants.hostName
+        
+        CFPreferencesSetValue(key, value, applicationID, userName, hostName)
+        let result = CFPreferencesSynchronize(applicationID, userName, hostName)
+        guard result else {
+            throw FontSmoothingDefaultsError.unknownError
+        }
+    }
+    
+    func getFontSmoothingState() throws -> FontSmoothingOptions {
+        guard let value = CFPreferencesCopyAppValue(CFPreferencesConstants.key, CFPreferencesConstants.applicationID) as? Int else {
+            throw FontSmoothingDefaultsError.unknownError
+        }
+        
+        guard let option = FontSmoothingOptions(rawValue: value) else {
+            throw FontSmoothingDefaultsError.unknownError
+        }
+        
+        return option
     }
     
     private enum FontSmoothingDefaultsError: Error {
         case unknownError
     }
     
-    func setFontSmoothing(option: FontSmoothingOptions) throws {
-        guard let args = setFontSmoothingStateArguments[option] else {
-            fatalError("Unsupported set font smoothing arguments option.")
-        }
-        
-        do {
-            let result = try runDefaultsCommand(with: args)
-            
-            if result.output != "" || result.error != "" {
-                throw FontSmoothingDefaultsError.unknownError
-            }
-        } catch {
-            if !error.localizedDescription.contains(domainDefaultPairDoesNotExistText) {
-                throw error
-            }
-        }
-    }
-    
-    func getFontSmoothingState() throws -> FontSmoothingOptions {
-        let result = try runDefaultsCommand(with: getFontSmoothingStateArguments)
-        
-        if result.error.contains(domainDefaultPairDoesNotExistText) {
-            return .defaultFontSmoothing
-        }
-        
-        switch result.output {
-        case "0\n":
-            return .noFontSmoothing
-        case "1\n":
-            return .lightFontSmoothing
-        case "2\n":
-            return .mediumFontSmoothing
-        case "3\n":
-            return .heavyFontSmoothing
-        default:
-            throw FontSmoothingDefaultsError.unknownError
-        }
-    }
-    
-    private func runDefaultsCommand(with arguments: ProcessArguments) throws -> DefaultsResult {
-        let task = Process()
-        
-        task.executableURL = defaultsUrl
-        task.arguments = arguments
-
-        let outputPipe = Pipe()
-        let errorPipe = Pipe()
-
-        task.standardOutput = outputPipe
-        task.standardError = errorPipe
-        
-        try task.run()
-        
-        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-
-        let output = String(decoding: outputData, as: UTF8.self)
-        let error = String(decoding: errorData, as: UTF8.self)
-        
-        let result = DefaultsResult(output: output, error: error)
-        return result
+    private struct CFPreferencesConstants {
+        static let key = "AppleFontSmoothing" as CFString
+        static let applicationID = kCFPreferencesAnyApplication
+        static let userName = kCFPreferencesCurrentUser
+        static let hostName = kCFPreferencesCurrentHost
     }
 }
