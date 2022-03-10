275c775e0f17e2e2d975398cf3ff4b841609dc1d
Alastair Byrne <alastairbyrne@gmail.com>
2022-03-10 10:55:30 +0000

    Update success modal copy (#26)
    * Let users know that closing and re-opening windows will apply the new font smoothing settings to those windows

--- Big Sur Font Smoothing Toggler/MainWindowController.swift
+++ Big Sur Font Smoothing Toggler/MainWindowController.swift
@@ -98,7 +98,7 @@ class MainWindowController: NSWindowController {
         guard let window = self.window else { return }
         let alert = NSAlert()
         alert.messageText = NSLocalizedString("Font smoothing preferences successfully updated", comment: "Title for preferences update success sheet modal")
-        alert.informativeText = NSLocalizedString("Log out or restart your Mac for the changes to take effect.", comment: "Informative text for preferences update success sheet modal")
+        alert.informativeText = NSLocalizedString("Log out or restart your Mac for the changes to take effect across all windows. You can also apply the new font smoothing setting to individual windows by closing and re-opening them.", comment: "Informative text for preferences update success sheet modal")
         alert.addButton(withTitle: NSLocalizedString("Log out now", comment: "Log out now button text"))
         alert.addButton(withTitle: NSLocalizedString("Log out later", comment: "Log out later button text"))
         alert.alertStyle = .informational
